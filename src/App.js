619d15209f7be1c3af130ff3995cd0110e9f3f6c
pgr101 <jim-pappas94@hotmail.com>
2021-06-18 11:41:32 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import './Styles/Button.css'
+import head from './Assets/Kaws-companion-galerie-1.jpg';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img className="Afbeelding" src={head}/>
+        <div className="Home">Home</div>
+        <div className="Library">Library</div>
+        <div className="Trade">Trade</div>
+        <div className="Faq">Faq</div>
+        <div className="Login">Login</div>
+        <div className="Toy1">Toy1</div>
+        <div className="Toy2">Toy2</div>
+        <div className="Toy3">Toy3</div>
+        <div className="Profile">Profile</div>
+        <div className="Socials">Socials</div>
+        <div className="Footer">Footer</div>
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Pages/Home.js
@@ -0,0 +1,3 @@
+import './App.css';
+import './Styles/Button.css'
+import head from './Assets/Kaws-companion-galerie-1.jpg';
